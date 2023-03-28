1c05a8ffb62c59b6b1d16081517181d4a2ef5b81
Sebastián Vélez Cortés <63685561+sebastianvelezcortes@users.noreply.github.com>
2023-03-28 11:03:55 -0500

    Update comandos-sql.sql

--- comandos-sql.sql
+++ comandos-sql.sql
@@ -1,2 +1,23 @@
 # En esta aula te hablaremos sobre la historia de SQL y de My SQL.
 # También instalaremos Workbench para poder utilizar un IDE en el desarrollo de nuestros ejercicios.
+
+# Ya tu ambiente debe estar listo para trabajar. Te invito ver los videos y a ejecutar todos los 
+# comandos que vas a aprender.
+
+#Los comandos vistos en el aula anterior son los siguientes:
+
+USE world;
+
+SELECT * FROM city;
+
+SELECT * FROM country;
+
+SELECT * FROM countrylanguage;
+
+CREATE DATABASE jugos;
+
+DROP SCHEMA jugos;
+
+SELECT * FROM CITY;
+
+select * from country;
